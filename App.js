6dda39502951b17162db753ace9d66ffb44b3893
ccooper2382 <93221305+ccooper2382@users.noreply.github.com>
2022-03-24 20:17:55 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import Temperature from "./Components/temperature";
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="w-60 mr-auto ml-auto text-center">
-        <Temperature/>
-    </div>
-  );
+class App extends Component {
+    state = {
+        setting: 15
+    }
+
+    handleIncrement = () => {
+        let number = this.state.setting;
+        let newNumber = number + 1;
+        this.setState({setting: newNumber})
+
+    }
+
+
+    handleDecrement = () => {
+        let number = this.state.setting;
+        let newNumber = number - 1;
+        this.setState({setting: newNumber})
+    }
+
+    render() {
+        return (
+            <div className="w-60 mr-auto ml-auto text-center">
+                <Temperature onIncrement={this.handleIncrement} onDecrement={this.handleDecrement} setting={this.state.setting}/>
+            </div>
+        );
+    }
+
 }
+
 
 export default App;
--- Components/temperature.jsx
+++ Components/temperature.jsx
@@ -1,23 +1,22 @@
-import React, {useState} from 'react';
 
-function Counter() {
 
-    const [count, setCount] = useState(15);
+const Counter = ({setting, onIncrement, onDecrement}) => {
+
     let temperatureCSS = "border-solid border-4 text-white rounded-full w-28 h-28 align-middle flex justify-center items-center ml-auto mr-auto ";
 
-    if (count === 15) {
+    if (setting >= 15 && setting <= 20 ) {
         temperatureCSS += "bg-blue-900";
     }
 
     return (
         <div>
-            <div className={temperatureCSS}>{count} C</div>
+            <div className={temperatureCSS}>{setting} C</div>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full  m-1"
-                    onClick={() => setCount(count + 1)}>
+                    onClick={onIncrement}>
                 <span>+</span>
             </button>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full h-full m-1"
-                    onClick={() => setCount(count - 1)}>
+                    onClick={onDecrement}>
                 <span >-</span>
             </button>
         </div>
